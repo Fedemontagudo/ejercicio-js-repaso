e3746c1765d19fb2159a566f18aa1cdd4ab3fcee
Fede Montagud <fedemontagudo@gmail.com>
2021-02-20 16:51:45 +0100

    correccion + cambio nombre variables funcion

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
-const rojo = [255];
-const verde = [193];
-const azul = [68];
+const rojo = 255;
+const verde = 193;
+const azul = 68;
 
-color(rojo, verde, azul)
+color(rojo, verde, azul);
 
-function color(rojo, verde, azul) {
-  let mensaje = "El color es R:" + rojo + ", G:" + verde + ", B:" + azul;
+function color(r, g, b) {
+  let mensaje = "El color es R:" + r + ", G:" + g + ", B:" + b;
   console.log(mensaje);
 }
 
