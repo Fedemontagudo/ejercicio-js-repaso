ed34dbb052ade9770200376b5c30f231a1db22b5
Fede Montagud <fedemontagudo@gmail.com>
2021-02-20 15:24:41 +0100

    Función funcionando

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+const rojo = [255];
+const verde = [193];
+const azul = [68];
 
+color(rojo, verde, azul)
+
+function color(rojo, verde, azul) {
+  let mensaje = "El color es R:" + rojo + ", G:" + verde + ", B:" + azul;
+  console.log(mensaje);
+}
 
 
